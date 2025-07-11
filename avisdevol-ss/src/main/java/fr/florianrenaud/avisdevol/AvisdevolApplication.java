35850431a4b2bf834175bdf60a960d399cef3546
Florian Renaud <florianrenaud31770@gmail.com>
2025-07-11 17:14:43 +0200

    Initial project setup

---
+++ avisdevol-ss/src/main/java/fr/florianrenaud/avisdevol/AvisdevolApplication.java
@@ -0,0 +1,13 @@
+package fr.florianrenaud.avisdevol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisdevolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisdevolApplication.class, args);
+	}
+
+}
---
+++ avisdevol-ss/src/test/java/fr/florianrenaud/avisdevol/AvisdevolApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.florianrenaud.avisdevol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisdevolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
